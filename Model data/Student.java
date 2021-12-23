b6d9e571813ca4fe09393281c4a527fccac64351
Grebennikov Igor <cidor361@gmail.com>
2021-12-23 22:27:57 +0300

    Created student class, ini and xml file

---
+++ Model data/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private int age;
+    private String height;
+    private String weight;
+
+    public void setFirstname(String firstname) {this.firstname = firstname;}
+    public void setMiddlename(String middlename) {this.middlename = middlename;}
+    public void setLastname(String lastname) {this.lastname = lastname;}
+    public void setAge(int age) {this.age = age;}
+    public void setHeight(int height) {this.height = height;}
+    public void setWeight(int weight) {this.weight = weight;}
+
+    public String getFirstname() {return firstname;}
+    public String getMiddlename() {return middlename;}
+    public String getLastname() {return lastname;}
+    public int getAge() {return age;}
+    public String getHeight() {return height;}
+    public String getWeight() {return weight;}
+}
